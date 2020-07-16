a56910e816aa7485d60859759d54ce443df5c890
sprihaak <68303040+sprihaak@users.noreply.github.com>
2020-07-16 14:38:55 -0400

    finished all of my todo items so code is now complete

--- skeleton_habituationGWR.py
+++ skeleton_habituationGWR.py
@@ -16,14 +16,15 @@ import matplotlib.pyplot as plt
 ###############################
 # TODO: Set a variable called stv to 4, this will define 
 #        the weight of the connection from input to output
-# stv = 
+stv = 4 
 
 dec=0.7 # set weight decrement for habituation
 
 pls=[0, 0, 1, 0, 0] # set up a pulse
 
 # TODO: then create a list of 6 pulses, called x, to use for input
-# x = 
+x = pls*6 + [0]*10 + pls*6
+
 
 v = stv # Set connection weight to start weight value
 
@@ -34,19 +35,30 @@ v = stv # Set connection weight to start weight value
 # TODO: Find the length of the input list (x)
 #        and assign its value to a new variable, nTs
 #        (which tracks the number of Time Steps)
-# nTs = 
+nTs = len(x)
 
 # TODO: Create a numpy array named y, a 1D vector
 #        (row vector) padded with zeros, with one
 #        element per each time step
-# y = 
+y = np.zeros((1, nTs))
 
 # TODO: use a for-loop to iterate 
 #        through each time step in 
 #        the input series and calculate
 #        the output at each time step. Ex:
-# for ...
-
+for t in range(nTs):
+    y[0,t]= v*x[t]
+    
+    if x[t]>0: # some stimulation
+        v = v*dec #reduce weight
+        
+    if t>2 and sum(x[t-4:t])==0 and v<stv:
+        v+=(stv-v)*.05 # let's forget this association we just
+        # learned if it doesn't happen for a while
+#     then indent 4 spaces and write the equation that
+#     describes how each input value in the vector x is 
+#     transformed to the output value in the vector y
+    
 #     then indent 4 spaces and write the equation that
 #     describes how each input value in the vector x is 
 #     transformed to the output value in the vector y
@@ -54,7 +66,9 @@ v = stv # Set connection weight to start weight value
 # TODO: At each time step, check if the gill is receiving
 #        stimulation and, if it is, cause the connection
 #        weight to habituate (decrease)
-   
+forgetflag= True #allows model to simulate the process of
+                    # forgetting the new association it learnt
+# if pulse isn't felt in like a million years, forget the stimualtion   
 
 
 ###############################
